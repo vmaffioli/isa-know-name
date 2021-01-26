dd0f6fee9af78e3d423929087f810cea8b4f9e9d
vmaffioli <vinimaffioli@gmail.com>
2021-01-25 21:27:23 -0300

    initial commit v1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# isa-know-name
+# isname
+
+A simple validation to check if a string is a know first name.
+
+At this point the main function just compares an input string with a json list loaded by the function. Currently with 7000+ know names.
+
+The focus of this project are build and upload a simple NPM Package, for studies pruporses.
+
+    ##Instalation -> 
+    npm install is-know-name 
+    
+    ##In code usage -> 
+    const name = require('is-know-name');
+    name('Put-a-name-here')
+
+    ##Input ->
+    String = Returns True or False;
+    Array = Returns an array with names that have passed validation; if nobody passes the validation, it returns false;
---
+++ index.js
@@ -0,0 +1,53 @@
+const names = require('./data.json');
+
+module.exports = (nameEntry) => {
+    const  nameList = Object.values(names);
+    let result = false;
+    let verif = false;
+    let resultList = [];
+
+
+    if (typeof(nameEntry) === 'string') {
+        verif = /\s/g.test(nameEntry)
+        nameEntry = nameEntry.trim().toLowerCase();
+        if (verif){
+            nameEntry = nameEntry.split(" ");
+        }
+        if (typeof(nameEntry) === 'object') {
+
+            for (let i=0;i < nameEntry.length;i++){
+
+                nameList.forEach(name => {
+                    if ((name.toLowerCase() === nameEntry[i].toLowerCase())) {
+                        resultList.push(nameEntry[i]);
+                    };
+                });
+            };
+            if (resultList.length > 0) {
+                result = true;
+            }
+        } else {
+            nameList.forEach(name => {
+                if (name.toLowerCase() === nameEntry) {
+                    result = true;
+                }
+            })
+        }
+    } else if ((typeof(nameEntry) === 'object') && (verif)) {
+        for (let i=0;i < nameEntry.length;i++){
+            nameList.forEach(name => {
+                if ((name.toLowerCase() === nameEntry[i].toLowerCase())) {
+                    resultList.push(nameEntry[i]);
+                };
+            });
+        };
+        if (resultList.length > 0) {
+            result = resultList
+        }
+    };
+    return result;
+}
+
+
+
+
